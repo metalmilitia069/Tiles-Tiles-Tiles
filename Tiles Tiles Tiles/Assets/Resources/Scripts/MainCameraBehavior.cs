0b4601457ce50ac603473962b7ad265e75ab2469
Rasputin <medina.engineering@outlook.com>
2020-06-04 22:16:25 -0400

    Main Camera  Behavior

---
+++ Tiles Tiles Tiles/Assets/Resources/Scripts/MainCameraBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraBehavior : MonoBehaviour
+{
+    public float cameraArm;
+    public Vector3 direction;
+    public GameObject cameraFocusPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.position = cameraFocusPoint.transform.position 
+        //Vector3.Distance(this.transform.position, cameraFocusPoint.transform.position) = cameraArm;
+    }
+}
